d3465e91bb8228a5848ccdbdda70a5b90f8f4aef
Rushikesh Jadhav <rhushikesh.jadhav@leaffintech.com>
2023-01-19 16:34:08 +0530

    new update

---
+++ src/Utils/geoCoords.js
@@ -0,0 +1,22 @@
+function geoChoords(){
+    return new Promise((resolve,reject) =>
+    {
+        if(window.navigator.geolocation)
+        {
+            window.navigator.geolocation.getCurrentPosition(
+                ({coords}) =>{
+                    resolve(coords);
+                },
+                (err)=>{
+                    reject(err);
+                }
+            );
+        }else{
+            console.log("Geo Location not supported");
+        }
+    });
+    
+}
+
+
+export default geoChoords;
